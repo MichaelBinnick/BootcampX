9447b86e5235f049bb2990ab89da77879f4045cb
Michael Binnick <mbinnick@live.ca>
2022-09-21 22:40:42 +0000

    implements SQL injection defenses

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -10,14 +10,16 @@ const pool = new Pool({
 const userInput = process.argv.slice(2);
 const cohortArg = userInput[0];
 const limitArg = userInput[1];
-
-pool.query(`
+const queryString = `
 SELECT students.id, students.name, cohorts.name as cohort_name
 FROM students
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = '${cohortArg}'
-LIMIT ${limitArg};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2
+`;
+const values = [`%${cohortArg}%`, limitArg];
+
+pool.query(queryString, values)
 .then(res => {
   console.log(res.rows);
 })
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,16 +9,18 @@ const pool = new Pool({
 
 const userInput = process.argv.slice(2);
 const cohortArg = userInput[0];
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`;
+const values = [`%${process.argv[2]}%`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
